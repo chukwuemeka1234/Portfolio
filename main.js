7c0675546d6a6ad2c564a0eda7f27147caa0efcd
abdollah raafat <abdollah.raafat.daif@gmail.com>
2021-11-16 19:02:46 +0200

    Style pop-up menu

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 const modal = document.querySelector('.nav-menu-link');
 
 document.getElementById('menu').addEventListener('click', () => {
-  modal.style.display = 'flex';
+  modal.style.width = '100%';
 });
 
 const close = document.getElementById('closeNav');
 close.onclick = function () {
-  modal.style.display = 'none';
+  modal.style.width = '0';
 };
 
 const portfolio = document.getElementById('portfolioPage');
 portfolio.onclick = function () {
-    modal.style.display = 'none';
+  modal.style.width = '0';
 };
 
 const about = document.getElementById('aboutPage');
 about.onclick = function () {
-    modal.style.display = 'none';
+  modal.style.width = '0';
 };
 
 const contact = document.getElementById('contactPage');
 contact.onclick = function () {
-    modal.style.display = 'none';
+  modal.style.width = '0';
 }
