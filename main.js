ae257e30dfcadbcc7f303bb16cbfa135dbd17c3b
chukwuemeka1234 <anivictor.chukwuemeka.avc@gmail.com>
2021-11-18 12:56:03 +0100

    Fix required changes

--- main.js
+++ main.js
@@ -24,49 +24,47 @@ contact.onclick = function () {
   modal.style.width = '0';
 };
 
-
-// popup window 
-
+// popup window
 const myProjects = [
 
-    {
-        name: 'Tonic',
-        description: `A daily selection of privately'personalized reads; no accounts or sign-ups
-        required.`,
-        featuredImage: './Image/SnapshootDesktopPortfolioA.svg',
-        technology: ['html', 'css', 'javascript', 'github', 'ruby', 'bootstrap'],
-        liveVersion: '#',
-        sourceCode: '#',
-      },
-    
-      {
-        name: 'Multi-Post Stories',
-        description: `Experimental content creation feature that allows users to add
-        to an existing story over the course of a day without
-        spamming their friends.`,
-        featuredImage: './Image/SnapshootDesktopPortfolioB.svg',
-        technology: ['html', 'css', 'javascript', 'github', 'ruby', 'bootstrap'],
-        liveVersion: '#',
-        sourceCode: '#',
-      },
-    
-      {
-        name: 'Facebook 360',
-        description: 'Exploring the future of media in Facebook\'s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR',
-        featuredImage: './Image/SnapshootDesktopPortfolioC.svg',
-        technology: ['html', 'css', 'javascript', 'github', 'ruby', 'bootstrap'],
-        liveVersion: '#',
-        sourceCode: '#',
-      },
-    
-      {
-        name: 'Uber Navigation',
-        description: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
-        featuredImage: './Image/SnapshootDesktopPortfolioD.svg',
-        technology: ['html', 'css', 'javascript', 'github', 'ruby', 'bootstrap'],
-        liveVersion: '#',
-        sourceCode: '#',
-      },
+  {
+    name: 'Tonic',
+    description: `A daily selection of privately'personalized reads; no accounts or sign-ups
+    required.`,
+    featuredImage: './Image/SnapshootDesktopPortfolioA.svg',
+    technology: ['html', 'css', 'javascript', 'github', 'ruby', 'bootstrap'],
+    liveVersion: '#',
+    sourceCode: '#',
+  },
+
+  {
+    name: 'Multi-Post Stories',
+    description: `Experimental content creation feature that allows users to add
+    to an existing story over the course of a day without
+    spamming their friends.`,
+    featuredImage: './Image/SnapshootDesktopPortfolioB.svg',
+    technology: ['html', 'css', 'javascript', 'github', 'ruby', 'bootstrap'],
+    liveVersion: '#',
+    sourceCode: '#',
+  },
+
+  {
+    name: 'Facebook 360',
+    description: 'Exploring the future of media in Facebook\'s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR',
+    featuredImage: './Image/SnapshootDesktopPortfolioC.svg',
+    technology: ['html', 'css', 'javascript', 'github', 'ruby', 'bootstrap'],
+    liveVersion: '#',
+    sourceCode: '#',
+  },
+
+  {
+    name: 'Uber Navigation',
+    description: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+    featuredImage: './Image/SnapshootDesktopPortfolioD.svg',
+    technology: ['html', 'css', 'javascript', 'github', 'ruby', 'bootstrap'],
+    liveVersion: '#',
+    sourceCode: '#',
+  },
 ];
 
 const seeProjectBtn = document.querySelectorAll('.popup-project');
